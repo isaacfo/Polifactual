0cc12f68f67c40238f0d2b31561c7d900ad42443
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-04 11:20:54 -0500

    gets rid of unneeded backend models and tables

--- server/schema.sql
+++ server/schema.sql
@@ -8,25 +8,3 @@ create table users (
     pwhash varchar(60) not null
 );
 
-
--- ========================================================
--- ALBUM: NAME, CODE
--- ========================================================
-
--- create table albums (
---   id serial primary key,
---   name text,
---   code text,
---   user_id integer references users (id) on delete cascade
--- );
-
--- ========================================================
--- IMAGES : URL 
--- ========================================================
-
-create table images (
-  id serial primary key,
-  url text,
-  users_id integer references users (id) on delete cascade
-  -- albums_id integer references albums (id) on delete cascade
-);
