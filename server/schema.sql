dbe553be1c6bd12d54e072a46359b20a01c272cd
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-04 23:35:39 -0500

    gets rid of unneeded code from schema

--- server/schema.sql
+++ server/schema.sql
@@ -9,24 +9,3 @@ create table users (
 );
 
 
--- ========================================================
--- ALBUM: NAME, CODE
--- ========================================================
-
--- create table albums (
---   id serial primary key,
---   name text,
---   code text,
---   user_id integer references users (id) on delete cascade
--- );
-
--- ========================================================
--- IMAGES : URL 
--- ========================================================
-
-create table images (
-  id serial primary key,
-  url text,
-  users_id integer references users (id) on delete cascade
-  -- albums_id integer references albums (id) on delete cascade
-);
