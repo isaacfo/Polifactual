4844f7d808a1764a334180dfcadf43df4664fbcb
hharris0803 <hchang0803@outlook.com>
2019-01-07 14:56:48 -0500

    small design tweaks to team page

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -4,125 +4,124 @@ import {  MDBRow, MDBCol, MDBCard, MDBAvatar, MDBCardBody, MDBIcon } from "mdbre
 class Team extends React.Component {
     render() {
       return (
-          // <div>hi</div>
-    <MDBCard className="my-5 px-5 pb-5 text-center">
-        <MDBCardBody>
-          <h2 className="h1-responsive font-weight-bold my-5">
-            Our amazing team
-          </h2>
-          <p className="grey-text w-responsive mx-auto mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-            error amet numquam iure provident voluptate esse quasi, veritatis
-            totam voluptas nostrum quisquam eum porro a pariatur veniam.
-          </p>
-          <MDBRow>
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              {/* <MDBAvatar
-                tag="img"
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              /> */}
-              <h5 className="font-weight-bold mt-4 mb-3">Helen Harris</h5>
-              <p className="text-uppercase blue-text">Software Engineer</p>
-              <p className="grey-text">
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci sed quia non numquam modi tempora eius.
-              </p>
-              <ul className="list-unstyled mb-0">
-              <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="envelope" className="blue-text" />
-              </a>
-              <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="linkedin" className="blue-text" />
+        <MDBCard className="my-5 px-5 pb-5 text-center">
+          <MDBCardBody>
+            <h2 className="h1-responsive font-weight-bold my-5">
+              Our amazing team
+            </h2>
+            <p className="grey-text w-responsive mx-auto mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+              error amet numquam iure provident voluptate esse quasi, veritatis
+              totam voluptas nostrum quisquam eum porro a pariatur veniam.
+            </p>
+            <MDBRow>
+              <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                {/* <MDBAvatar
+                  tag="img"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg"
+                  className="rounded-circle z-depth-1 img-fluid"
+                  alt="Sample avatar"
+                /> */}
+                <h5 className="font-weight-bold mt-4 mb-3">Helen Harris</h5>
+                <p className="text-uppercase blue-text">Software Engineer</p>
+                <p className="grey-text">
+                  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                  consectetur, adipisci sed quia non numquam modi tempora eius.
+                </p>
+                <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="envelope" className="blue-text" />
                 </a>
                 <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="github" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
+                    <MDBIcon icon="linkedin" className="blue-text" />
+                  </a>
+                  <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="github" className="blue-text" />
+                  </a>
+                </ul>
+              </MDBCol>
 
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              {/* <MDBAvatar
-                tag="img"
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              /> */}
-              <h5 className="font-weight-bold mt-4 mb-3">Brien Mizell</h5>
-              <p className="text-uppercase blue-text">Software Engineer</p>
-              <p className="grey-text">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                ipsa accusantium doloremque rem laudantium totam aperiam.
-              </p>
-              <ul className="list-unstyled mb-0">
-              <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="envelope" className="blue-text" />
-              </a>
-              <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="linkedin" className="blue-text" />
+              <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                {/* <MDBAvatar
+                  tag="img"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
+                  className="rounded-circle z-depth-1 img-fluid"
+                  alt="Sample avatar"
+                /> */}
+                <h5 className="font-weight-bold mt-4 mb-3">Brien Mizell</h5>
+                <p className="text-uppercase blue-text">Software Engineer</p>
+                <p className="grey-text">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  ipsa accusantium doloremque rem laudantium totam aperiam.
+                </p>
+                <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="envelope" className="blue-text" />
                 </a>
                 <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="github" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
+                    <MDBIcon icon="linkedin" className="blue-text" />
+                  </a>
+                  <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="github" className="blue-text" />
+                  </a>
+                </ul>
+              </MDBCol>
 
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              {/* <MDBAvatar
-                tag="img"
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              /> */}
-              <h5 className="font-weight-bold mt-4 mb-3">Kyle Sekellick</h5>
-              <p className="text-uppercase blue-text">Software Engineer</p>
-              <p className="grey-text">
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim est fugiat nulla id eu laborum.
-              </p>
-              <ul className="list-unstyled mb-0">
-              <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="envelope" className="blue-text" />
-              </a>
+              <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                {/* <MDBAvatar
+                  tag="img"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
+                  className="rounded-circle z-depth-1 img-fluid"
+                  alt="Sample avatar"
+                /> */}
+                <h5 className="font-weight-bold mt-4 mb-3">Kyle Sekellick</h5>
+                <p className="text-uppercase blue-text">Software Engineer</p>
+                <p className="grey-text">
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim est fugiat nulla id eu laborum.
+                </p>
+                <ul className="list-unstyled mb-0">
                 <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="linkedin" className="blue-text" />
+                    <MDBIcon icon="envelope" className="blue-text" />
                 </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="github" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
+                  <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="linkedin" className="blue-text" />
+                  </a>
+                  <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="github" className="blue-text" />
+                  </a>
+                </ul>
+              </MDBCol>
 
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              {/* <MDBAvatar
-                tag="img"
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-              /> */}
-              <h5 className="font-weight-bold mt-4 mb-3">Isaac Fonseca</h5>
-              <p className="text-uppercase blue-text">BSoftware Engineer</p>
-              <p className="grey-text">
-                Perspiciatis repellendus ad odit consequuntur, eveniet earum
-                nisi qui consectetur totam officia voluptates perferendis
-                voluptatibus aut.
-              </p>
-              <ul className="list-unstyled mb-0">
-              <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="envelope" className="blue-text" />
-              </a>
+              <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                {/* <MDBAvatar
+                  tag="img"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
+                  className="rounded-circle z-depth-1 img-fluid"
+                  alt="Sample avatar"
+                /> */}
+                <h5 className="font-weight-bold mt-4 mb-3">Isaac Fonseca</h5>
+                <p className="text-uppercase blue-text">Software Engineer</p>
+                <p className="grey-text">
+                  Perspiciatis repellendus ad odit consequuntur, eveniet earum
+                  nisi qui consectetur totam officia voluptates perferendis
+                  voluptatibus aut.
+                </p>
+                <ul className="list-unstyled mb-0">
                 <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="linkedin" className="blue-text" />
+                    <MDBIcon icon="envelope" className="blue-text" />
                 </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon icon="github" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
-          </MDBRow>
-        </MDBCardBody>
-      </MDBCard>
+                  <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="linkedin" className="blue-text" />
+                  </a>
+                  <a href="#!" className="p-2 fa-lg">
+                    <MDBIcon icon="github" className="blue-text" />
+                  </a>
+                </ul>
+              </MDBCol>
+            </MDBRow>
+          </MDBCardBody>
+        </MDBCard>
   );
 }
 }
