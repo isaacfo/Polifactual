5c033d6f9e9d9873e6da195df3b54afaf25324e9
Isaac Fonseca <isaacfo1010@gmail.com>
2019-01-08 12:05:10 -0500

    added address to both seed and schema

--- server/seed.sql
+++ server/seed.sql
@@ -1,44 +1,13 @@
 -- ============================================================================
---                  USERS: NAME, USERNAME, PWHASH
+--                  USERS: NAME,ADDRESS USERNAME, PWHASH
 -- ============================================================================
 insert into users
-    (name, username, pwhash)
+    (name, username, address, pwhash)
 values
-    ('NAME1', 'USERNAME1', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
-    ('NAME2', 'USERNAME2', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
-    ('NAME3', 'USERNAME3', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+    ('NAME1', 'USERNAME1','ADDRESS1', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
+    ('NAME2', 'USERNAME2','ADDRESS2', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
+    ('NAME3', 'USERNAME3','ADDRESS3'. '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
 
-
-
-
-
--- ============================================================================
---                  ALBUMS
--- ============================================================================
--- insert into albums
---     (name, code)
--- values
---     ('insert into albums1', ''),
---     ('insert into albums2', ''),
---     ('insert into albums3', ''),
---     ('insert into albums4', ''),
---     ('insert into albums5', '');
-
-
-
-
-
--- ============================================================================
---                  IMAGES
--- ============================================================================
-insert into images
-    (url, users_id)
-values
-    ('/images/1.png', 1),
-    ('/images/2.png', 1),
-    ('/images/3.png', 1),
-    ('/images/4.png', 2),
-    ('/images/5.png', 2);
 
 
 
